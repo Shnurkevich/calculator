4f5b45b53cca0d22090592f2606b48fda410a989
Aliaksandr Shnurkevich <65668318+Shnurkevich@users.noreply.github.com>
2021-08-01 23:30:02 +0300

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,52 @@
+package com.calculator;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        Operation operation = new Operation();
+        Scanner scanner = new Scanner(System.in);
+        boolean exitProgram = true;
+
+            do {
+                System.out.println("Выберите операцию введя её цифру \n 1 - Сложение" +
+                        "\n 2 - Вычитание \n 3 - Умножение \n 4 - Деление \n 5 - Выход из приложения");
+
+                String input = scanner.nextLine();
+                try{
+                    switch (input) {
+                    case ("1"):
+                        System.out.println("Сложение");
+                        operation.plus();
+                        break;
+                    case ("2"):
+                        System.out.println("Вычитание");
+                        operation.minus();
+                        break;
+                    case ("3"):
+                        System.out.println("Умножение");
+                        operation.multiplication();
+                        break;
+                    case ("4"):
+                        System.out.println("Деление");
+                        operation.division();
+                        break;
+                    case ("5"):
+                        System.out.println("Выход");
+                        exitProgram = false;
+                        break;
+                    default:
+                        System.out.println("Неправильный ввод.");
+                    }
+                }
+                catch (InputMismatchException e) {
+                    System.out.println("Неверный ввод, введите число");
+                }
+            }
+
+            while (exitProgram);
+
+    }
+}
---
+++ Operation.java
@@ -0,0 +1,52 @@
+package com.calculator;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Operation {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public String plus() {
+
+        System.out.println("Введите первое число");
+        int num1 = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int num2 = scanner.nextInt();
+        int result = num1 + num2;
+        System.out.println("Результат сложения = " + result);;
+        return Integer.toString(result);
+    }
+
+    public String minus() {
+        System.out.println("Введите первое число");
+        int num1 = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int num2 = scanner.nextInt();
+        int result = num1 - num2;
+        System.out.println("Результат вычитания = " + result);;
+        return Integer.toString(result);
+    }
+
+    public String multiplication() {
+        System.out.println("Введите первое число");
+        int num1 = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int num2 = scanner.nextInt();
+        int result = num1 * num2;
+        System.out.println("Результат умножения = " + result);;
+        return Integer.toString(result);
+    }
+
+    public String division() {
+        System.out.println("Введите первое число");
+        double num1 = scanner.nextDouble();
+        System.out.println("Введите второе число");
+        double num2 = scanner.nextInt();
+        double result = num1 / num2;
+        System.out.println("Результат деления = " + result);;
+        return Double.toString(result);
+    }
+
+}
+
